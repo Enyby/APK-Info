cba8663f9aaa90ee1bd7c1350e1eae48e354fb5b
Enyby <enyby@yandex.ru>
2018-11-08 07:17:06 +0200

    Fix shell integration: #9 Use only HKCU. Fix names. Add aditional command in context menu.

--- APK-Info-Shell-Integration.bat
+++ APK-Info-Shell-Integration.bat
@@ -3,14 +3,14 @@
 ECHO APK-Info shell integration...
 ECHO.
 
-REG ADD "HKCR\.apk" /ve /t REG_SZ /d "APK-Info" /f >nul 2>nul
-REG ADD "HKCR\APK-Info\DefaultIcon" /ve /t REG_SZ /d "\"%cd%\APK-Info.exe\"" /f >nul 2>nul
-REG ADD "HKCR\APK-Info\shell\APKInfo" /ve /t REG_SZ /d "APK-Info" /f >nul 2>nul
-REG ADD "HKCR\APK-Info\shell\APKInfo\command" /ve /t REG_SZ /d "\"%cd%\APK-Info.exe\" \"%%1\"" /f >nul 2>nul
-REG ADD "HKCU\SOFTWARE\Classes\.apk" /ve /t REG_SZ /d "APK-Info" /f >nul 2>nul
-REG ADD "HKCU\SOFTWARE\Classes\APK-Info\DefaultIcon" /ve /t REG_SZ /d "\"%cd%\APK-Info.exe\"" /f >nul 2>nul
-REG ADD "HKCU\SOFTWARE\Classes\APK-Info\shell\APKInfo" /ve /t REG_SZ /d "APK-Info" /f >nul 2>nul
-REG ADD "HKCU\SOFTWARE\Classes\APK-Info\shell\APKInfo\command" /ve /t REG_SZ /d "\"%cd%\APK-Info.exe\" \"%%1\"" /f >nul 2>nul
+for %%H in (HKCU) do (
+	REG ADD "%%H\SOFTWARE\Classes\.apk" /ve /t REG_SZ /d "APK-Info" /f >nul 2>nul
+	REG ADD "%%H\SOFTWARE\Classes\APK-Info\DefaultIcon" /ve /t REG_SZ /d "\"%cd%\APK-Info.exe\"" /f >nul 2>nul
+	REG ADD "%%H\SOFTWARE\Classes\APK-Info\shell\open" /ve /t REG_SZ /d "APK-Info" /f >nul 2>nul
+	REG ADD "%%H\SOFTWARE\Classes\APK-Info\shell\open\command" /ve /t REG_SZ /d "\"%cd%\APK-Info.exe\" \"%%1\"" /f >nul 2>nul
+	
+	REG ADD "%%H\SOFTWARE\Classes\SystemFileAssociations\.apk\Shell\APK-Info\Command" /ve /t REG_SZ /d "\"%cd%\APK-Info.exe\" \"%%1\"" /f >nul 2>nul
+)
 
 ECHO APK-Info shell integration completed!
 ECHO.
